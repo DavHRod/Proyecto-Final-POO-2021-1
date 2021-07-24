d9b83b38e3823593e775b273e0019d08aa0cb9f3
David Humberto Rodriguez Venegas <Drodriguezve@unal.edu.co>
2021-07-23 23:57:54 -0500

    Update sql, User
    sql y User en estado medio :shipit:

--- User.py
+++ User.py
@@ -3,9 +3,14 @@ from PyQt5.QtGui import QIntValidator
 from Afiliados import *
 from PyQt5 import QtWidgets 
 from principal import Ui_principal
+
 from afiliadoUi import Ui_afil_win
 from menu_afiliado import Ui_afil_menu
+from edit_afil import Ui_Dialog_edit
+
+from sql import sql
 import sys
+
 class principal_win(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -23,19 +28,56 @@ class afil_menu(QtWidgets.QMainWindow):
         super().__init__()
         self.a_ui = afiliado_win()
         self.a_menu = Ui_afil_menu()
+        self.a_edit = afil_edit()
         self.a_menu.setupUi(self)
         self.a_menu.push_nuevo.clicked.connect(self.show_afil)
+        self.a_menu.push_editar.clicked.connect(self.show_edit)
+        self.a_menu.push_regresar.clicked.connect(self.show_principal)
+    
+    def show_principal(self):
+        self.hide()
+
+    def show_edit(self):
+        self.hide()
+        self.a_edit.show()
 
     def show_afil(self):
         self.hide()
         self.a_ui.show()
 
+class afil_edit(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.a_edit = Ui_Dialog_edit()
+        self.a_ui = afiliado_win()
+        self.sql = sql()
+        self.a_edit.setupUi(self)
+        self.tupla = self.sql.cargar_tabla("Afiliados")
+        self.fill_combo()
+        self.a_edit.push_aceptar.clicked.connect(self.show_afil)
+    
+    def show_afil(self):
+        self.close()
+        self.a_ui.set_lines(self.tupla[self.a_edit.combo_carga.currentIndex()])
+        self.a_ui.show()
+
+    def back(self):
+        self.close()
+        self.a_menu.show()
+
+    def fill_combo(self):
+        index = 1
+        for x in self.tupla:
+            self.a_edit.combo_carga.insertItem(index, str(x[0]))
+            index += 1
+        
+
 class afiliado_win(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        self.sql = sql()
         self.a_ui = Ui_afil_win()
         self.a_ui.setupUi(self)
-        self.set_lines()
         self.a_ui.line_show_date.setText(self.a_ui.calendar_nacimiento.selectedDate().toString("dd/MM/yyyy"))
         self.a_ui.push_guardar.clicked.connect(self.btn_guardar)
         self.a_ui.calendar_nacimiento.setMaximumDate(QDate.currentDate())
@@ -47,12 +89,13 @@ class afiliado_win(QtWidgets.QMainWindow):
     def set_line_date(self):
         self.a_ui.line_show_date.setText(self.a_ui.calendar_nacimiento.selectedDate().toString("dd/MM/yyyy"))    
     
-    def set_lines(self):
-        afil = afiliado("", "", "", "", "", "", "", "", f"{datetime.today().day}/{datetime.today().month}/{datetime.today().year}", "", "", "", "")
-        self.a_ui.line_ide.setText(afil.get_ide())
+    def set_lines(self,carga):
+        afil = afiliado(carga[0], carga[1], carga[2], carga[3], carga[4], carga[5], carga[6], carga[7], carga[8], carga[9], carga[10], carga[11], carga[12])
+        self.a_ui.line_ide.setText(str(afil.get_ide()))
+        self.a_ui.line_nombre.setText(afil.get_nombre())
         self.a_ui.line_apellido.setText(afil.get_apellido())
         self.a_ui.line_direccion.setText(afil.get_direccion())
-        self.a_ui.line_telefono.setText(afil.get_telefono())
+        self.a_ui.line_telefono.setText(str(afil.get_telefono()))
         self.a_ui.line_correo.setText(afil.get_correo())
         self.a_ui.line_ciudad.setText(afil.get_ciudad())
         
@@ -62,12 +105,12 @@ class afiliado_win(QtWidgets.QMainWindow):
             vacunado = "Si"
         else:
             vacunado = "-"
-        datos = {"ide":int(self.a_ui.line_ide.text()), 
+        datos = {"ide":self.a_ui.line_ide.text(), 
             "id_plan":"-", 
             "nombre":self.a_ui.line_nombre.text(), 
             "apellido":self.a_ui.line_apellido.text(),
             "direccion":self.a_ui.line_direccion.text(),
-            "telefono":int(self.a_ui.line_telefono.text()),
+            "telefono":self.a_ui.line_telefono.text(),
             "correo":self.a_ui.line_correo.text(),
             "ciudad":self.a_ui.line_ciudad.text(),
             "nacimiento":self.a_ui.line_show_date.text(),
@@ -89,4 +132,6 @@ class afiliado_win(QtWidgets.QMainWindow):
             datos["afiliacion"],
             datos["desafiliacion"],
             datos["vacunado"])
-        print (afilN.to_tuple())
+        self.sql.guardar_tabla("Afiliados",afilN.to_tuple())
+
+
--- sql.py
+++ sql.py
@@ -4,6 +4,7 @@ from sqlite3 import Error
 class sql:
     def __init__(self):
         self.__con = self.__sql_connection()
+        self.__cursor=self.__con.cursor()
         self.__sql_table()
 
     def get_con (self):
@@ -18,10 +19,32 @@ class sql:
         except Error:
             print(Error)
 
+    def cargar_tabla(self, tabla, columna = "*"):
+        self.__cursor.execute(f"SELECT {columna} FROM {tabla}")
+        tupla = self.__cursor.fetchall()
+        return tupla
+    
+    def guardar_tabla(self, tabla, carga):
+        try:
+            self.__cursor.execute(f"INSERT INTO {tabla} VALUES {carga}")
+        except:
+            if tabla == "Afiliados":
+                carga_update = (carga[1], carga[2], carga[3], carga[4], carga[5], carga[6], carga[7], carga[8], carga[9], carga[10], carga[11], carga[12] )
+                self.__cursor.execute(f"UPDATE Afiliados SET ID_Plan = ? , Nombre = ?, Apellido = ? , Direccion = ? , Telefono = ? , Correo = ? , Ciudad_De_Residencia = ? , Fecha_De_Nacimiento = ? , Edad = ? , Fecha_De_Afiliacion = ?,Fecha_De_Desafiliacion = ? , Vacunado = ? WHERE Numero_De_Identificacion = {carga[0]}",carga_update)
+            elif tabla == "Planes":
+                carga_update = (carga[1], carga[2], carga[3], carga[4])
+                self.__cursor.execute(f"UPDATE Planes SET Edad_Min = ?, Edad_Max = ?, Fecha_inicio = ?, Fecha_Fin = ? WHERE ID = {carga[0]}",carga_update)
+            elif tabla == "Lotes":
+                carga_update = (carga[1], carga[2], carga[3], carga[4], carga[5], carga[6], carga[7], carga[8], carga[9], carga[10], carga[11] )
+                self.__cursor.execute(f"UPDATE Lotes SET Fabricante = ? , Tipo_De_Vacuna = ?, Cantidad_Recibida = ? , Cantidad_Asignada = ? , Cantidad_Usada = ? , Dosis_Necesarias = ? , Temperatura_De_Almacenamiento = ? , Efectividad_Identificada = ? , Tiempo_De_Proteccion = ? , Fecha_De_Vencimiento = ?,Imagen = ? WHERE Codigo_De_Lote = {carga[0]}",carga_update)
+            elif tabla == "Citas":
+                carga_update = (carga[1], carga[2], carga[3], carga[4], carga[5], carga[6])
+                self.__cursor.execute(f"UPDATE Afiliados SET ID_Plan = ? , Numero_De_Identificacion = ?, Ciudad_De_Vacunacion = ? , Codigo_De_Lote = ? , Fecha_De_Cita = ? , Hora_De_Cita = ? WHERE Numero_De_Cita = {carga[0]}",carga_update)
+        self.__con.commit()
+
     def __sql_table(self):
-        cursorObj = self.__con.cursor()
         #Creación de la tabla Afiliados
-        cursorObj.execute('''CREATE TABLE IF NOT EXISTS 
+        self.__cursor.execute('''CREATE TABLE IF NOT EXISTS 
         Afiliados(
             Numero_De_Identificacion integer PRIMARY KEY, 
             ID_Plan integer FOREING KEY,
@@ -38,7 +61,7 @@ class sql:
             Vacunado text)''')
 
         #Creación de la tabla Lotes
-        cursorObj.execute('''CREATE TABLE IF NOT EXISTS 
+        self.__cursor.execute('''CREATE TABLE IF NOT EXISTS 
         Lotes(
             Codigo_De_Lote integer PRIMARY KEY,
             Fabricante text, 
@@ -54,7 +77,7 @@ class sql:
             Imagen text)''')
 
         #Creación de la tabla Planes
-        cursorObj.execute('''CREATE TABLE IF NOT EXISTS 
+        self.__cursor.execute('''CREATE TABLE IF NOT EXISTS 
         Planes(
             ID integer PRIMARY KEY, 
             Edad_Min integer, 
@@ -63,7 +86,7 @@ class sql:
             Fecha_Fin text)''')
 
         #Creación de la tabla Citas
-        cursorObj.execute('''CREATE TABLE IF NOT EXISTS
+        self.__cursor.execute('''CREATE TABLE IF NOT EXISTS
         Citas(
             Numero_De_Cita integer PRIMARY KEY,
             ID_Plan integer,
@@ -72,3 +95,6 @@ class sql:
             Codigo_De_Lote integer,
             Fecha_De_Cita text,
             Hora_De_Cita integer)''')
+
+    def close(self):
+        self.__con.close()
